5ef91cefadb60790436f9223de93b6ae4b5f9ea5
Sara Reid <sara.reid.2015@uni.strath.ac.uk>
2017-10-08 21:50:33 +0100

    Created an interface for Accounts and three basic classes for accounts to be modified when we decide on different types of accounts, S.

---
+++ BankSystem/src/Bank2.java
@@ -0,0 +1,56 @@
+public class Bank2 implements AccountsI {
+    private double balance;
+    private int accNumber;
+
+    public Bank2(int acc, double bal){
+        accNumber = acc;
+        balance = bal;
+    }
+
+    @Override
+    public void deposit(double dep) {
+        balance = balance + dep;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if(balance >= amount) {
+            balance = balance - amount;
+            return true;
+        }
+        else
+            return false;
+    }
+
+
+    @Override
+    public void transfer() {
+
+    }
+
+    @Override
+    public double checkBal() {
+        return balance;
+    }
+
+    @Override
+    public void printBal() {
+
+    }
+
+    @Override
+    public void createAcc() {
+
+    }
+
+    @Override
+    public void deleteAcc() {
+
+    }
+
+    @Override
+    public void editAcc() {
+
+    }
+
+}
