8cc5c2bb6f46f5f3c2a61315b4a9f4d288333863
AHC <AHC>
2017-10-10 00:06:07 +0100

    expanded req doc for more info on bank accounts

--- threadingProj/src/bankSystem/AccountsI.java
+++ threadingProj/src/bankSystem/AccountsI.java
@@ -3,9 +3,11 @@ package bankSystem;
 public interface AccountsI {
     public void deposit(double dep);
     public boolean withdraw(double amount);
-    public void transfer();
+    public void transfer(); //takes in two account numbers?
     public double checkBal();
     public void printBal();
+
+    // not sure should be in this class maybe in the customer class?
     public void createAcc();
     public void deleteAcc();
     public void editAcc();
