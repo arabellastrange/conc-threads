1b89b85a3232031153186f44b054e33f6cdd1aac
Sara <szb15123@strath.ac.uk>
2017-10-10 16:50:34 +0100

    Current Account

--- threadingProj/src/bankSystem/AccountsI.java
+++ threadingProj/src/bankSystem/AccountsI.java
@@ -3,7 +3,8 @@ package bankSystem;
 public interface AccountsI {
     public void deposit(double dep);
     public boolean withdraw(double amount);
-    public void transfer();
+    public void transfer(double amount);
     public double checkBal();
     public void printBal();
+
 }
--- threadingProj/src/bankSystem/Customer.java
+++ threadingProj/src/bankSystem/Customer.java
@@ -1,12 +1,11 @@
 package bankSystem;
 
 public class Customer {
-<<<<<<< HEAD
+
 
     public void editAcc(){
 
     }
 }
-=======
-}
->>>>>>> e63d9dbebcd223a4273592ddb9d04477f871b1b9
+
+
