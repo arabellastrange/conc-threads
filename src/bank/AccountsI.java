62d226aa2fa56d792078e06f7fa322ab2e31955f
Chadha Degachi SE2015 <wyb15135@muir-16.cis.strath.ac.uk>
2017-10-17 14:41:35 +0100

    trying to get a class diagram

--- src/bank/AccountsI.java
+++ src/bank/AccountsI.java
@@ -4,11 +4,11 @@ public interface AccountsI { // change to inheritance to avoid code repeat?
     //move money - write operations
     public void deposit(double dep);
     public boolean withdraw(double amount);
-
-    public void transfer(double amount); // needs to take in an account number to transfer to as well
+    public void transfer(double amount);
+    // needs to take in an account number to transfer to as well
+    //public void transfer(int toAccountNum, double amount);
 
     //balance info - read operations
-
     public double checkBal();
     public void printBal();
 
