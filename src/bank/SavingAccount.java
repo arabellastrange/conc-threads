d6e54d3ee1b3c4a3c9951aa7dab1d3107c939ed8
Sandeep <sandeep.dhesi.2015@uni.strath.ac.uk>
2017-10-23 20:59:58 +0100

    Filling in a few methods on SavingAccount class

--- src/bank/SavingAccount.java
+++ src/bank/SavingAccount.java
@@ -1,48 +1,76 @@
 package bank;
 
 public class SavingAccount implements AccountsI {
+
+    private double balance;
+    private int accNumber;
+    private double interestRate;
+    private double interest;
+    private int sortCode;
+
+
+    public SavingAccount(int acc, double bal, double rate, double intRate){
+        accNumber = acc;
+        balance = bal;
+        interestRate = rate;
+    }
+
+
+
     @Override
     public void deposit(double dep) {
-
+        balance = balance + dep;
     }
 
     @Override
     public boolean withdraw(double amount) {
-        return false;
+        if (balance >= amount) {
+            balance = balance - amount;
+            return true;
+        } else
+            System.out.println("Insufficient funds.");
+            return false;
     }
 
     @Override
     public void transfer(double amount) {
-
+        if (amount <= this.balance) {
+            withdraw(amount);
+            // CurrentAccount.deposit(amount);
+            System.out.print("\nTransfer successful. Transferred: £" + amount);
+        } else {
+            System.out.print("\nTransfer failed, not enough balance!");
+        }
     }
 
     @Override
     public double checkBal() {
-        return 0;
+        return balance;
     }
 
     @Override
-    public void printBal() {
+    public void printBal()  {
 
+        System.out.print("Account number " + accNumber + " has the balance of " + balance);
     }
 
     @Override
     public int getAccountNumber() {
-        return 0;
+        return accNumber;
     }
 
     @Override
     public int getAccountSort() {
-        return 0;
+        return sortCode;
     }
 
     @Override
-    public void setInterestRate(double interestRate) {
-
+    public void setInterestRate(double rate) {
+        interest = balance * rate;
     }
 
     @Override
     public double getInterestRate() {
-        return 0;
+        return interest;
     }
 }
