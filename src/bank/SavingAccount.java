71bb510c5aa65b8f5727319c6d8b63c63ec820ec
AHC <AHC>
2017-10-23 22:32:34 +0100

    added changes from master branch!

--- src/bank/SavingAccount.java
+++ src/bank/SavingAccount.java
@@ -8,17 +8,28 @@ public class SavingAccount extends Account {
 
     @Override
     public void deposit(double dep) {
-
+        //should have upper limit!!
+        balance = balance + dep;
     }
 
     @Override
     public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance = balance - amount;
+            return true;
+        } else
+            System.out.println("Insufficient funds.");
         return false;
     }
 
     @Override
     public void transfer(double amount, int AccNum) {
-
+        if (amount <= this.balance) {
+            withdraw(amount);
+            System.out.print("\nTransfer successful. Transferred: £" + amount);
+        } else {
+            System.out.print("\nTransfer failed, not enough balance!");
+        }
     }
 
 }
