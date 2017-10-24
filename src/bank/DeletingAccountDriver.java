d8d96caf6854abac86718a8372306de62a7221ee
Sara Reid CS2015 <sara.reid.2015@uni.strath.ac.uk>
2017-10-24 16:29:05 +0100

    Update DeletingAccountDriver.java

--- src/bank/DeletingAccountDriver.java
+++ src/bank/DeletingAccountDriver.java
@@ -1,17 +1,18 @@
 package bank;
 
-public class DeletingAccountDriver {
+public class DeletingAccountDriver implements Runnable{
     public static void main(String[] args){
-        (new Thread(new ExampleDriver())).start();
+        (new Thread(new DeletingAccountDriver())).start();
     }
 
     public void run(){
-        BankSystem bank = BankSystem.getBank();
-        Customer c = new Customer("Mary", bank.getBank().getEmployee(0));
-        bank.getBank().addCustomer(c);
+        Customer c = new Customer("Mary", BankSystem.getBank().getEmployee(0));
+        BankSystem.getBank().addCustomer(c);
         Account a = new CurrentAccount(380, 0.02, 1);
-        bank.getBank().tellMeAboutBank();
-        bank.removeAccount(c,a);
+        BankSystem.getBank().addAccount(c,a);
+        BankSystem.getBank().tellMeAboutBank();
+        BankSystem.getBank().removeAccount(c,a);
+        BankSystem.getBank().tellMeAboutBank();
 
 
 
