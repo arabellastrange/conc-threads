22a1d916a8a6f38b0dfc121be3716df46fb2afad
Sara <szb15123@strath.ac.uk>
2017-10-24 16:22:51 +0100

    attempting deleting account driver

--- src/bank/DeletingAccountDriver.java
+++ src/bank/DeletingAccountDriver.java
@@ -1,4 +1,21 @@
 package bank;
 
 public class DeletingAccountDriver {
+    public static void main(String[] args){
+        (new Thread(new ExampleDriver())).start();
+    }
+
+    public void run(){
+        BankSystem bank = BankSystem.getBank();
+        Customer c = new Customer("Mary", bank.getBank().getEmployee(0));
+        bank.getBank().addCustomer(c);
+        Account a = new CurrentAccount(380, 0.02, 1);
+        bank.getBank().tellMeAboutBank();
+        bank.removeAccount(c,a);
+
+
+
+    }
+
+
 }
